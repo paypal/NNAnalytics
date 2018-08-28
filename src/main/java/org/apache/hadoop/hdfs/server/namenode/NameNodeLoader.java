92ac211299a186e11b51e60fb4d3d08431b1e592
Lukas Majercak <lukmajercak@gmail.com>
2018-08-28 15:24:25 -0700

    Update NameNodeLoader.java

--- src/main/java/org/apache/hadoop/hdfs/server/namenode/NameNodeLoader.java
+++ src/main/java/org/apache/hadoop/hdfs/server/namenode/NameNodeLoader.java
@@ -321,7 +321,7 @@ public class NameNodeLoader {
       GSet<INode, INodeWithAdditionalFields> preloadedInodes,
       Configuration preloadedHadoopConf,
       SecurityConfiguration nnaConf)
-      throws IOException, NoSuchFieldException, IllegalAccessException {
+      throws IOException, NoSuchFieldException, IllegalAccessException, URISyntaxException {
     /*
      * Configuration standard is: /etc/hadoop/conf.
      * Goal is to let configuration tell us where the FsImage and EditLogs are for loading.
