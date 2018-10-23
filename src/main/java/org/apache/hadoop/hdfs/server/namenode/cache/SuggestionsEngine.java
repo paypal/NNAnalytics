a889c5a55c1e292b9902899ac5356b8807b069d2
pljeliazkov <pljeliazkov@paypal.com>
2018-10-23 15:36:27 -0700

    [#142] SpotlessApply

--- src/main/java/org/apache/hadoop/hdfs/server/namenode/cache/SuggestionsEngine.java
+++ src/main/java/org/apache/hadoop/hdfs/server/namenode/cache/SuggestionsEngine.java
@@ -27,7 +27,6 @@ import java.sql.SQLException;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
